8d411bf83a02b06bb075cc1c5ce06a6d25ce1d42
Ian Rones <ronesp.85@gmail.com>
2021-07-11 20:41:47 -0700

    [add] included map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import ReactMapGL from "react-map-gl"
+import * as React from 'react';
+import ReactMapGL, {FlyToInterpolator} from 'react-map-gl';
+// 3rd-party easing functions
+// import d3 from 'd3-ease';
 
-function App() {
+export default function App() {
+  const [viewport, setViewport] = React.useState({
+    width: 800,
+    height: 600,
+    latitude: 37.78,
+    longitude: -122.45,
+    zoom: 14
+  });
+
+  const goToNYC = () => {
+    setViewport({
+      ...viewport,
+      longitude: -74.1,
+      latitude: 40.7,
+      zoom: 14,
+      transitionDuration: 5000,
+      transitionInterpolator: new FlyToInterpolator(),
+      // transitionEasing: d3.easeCubic
+    });
+  };
+
   return (
     <div>
-      <ReactMapGL>
-        
-      </ReactMapGL>
+      <ReactMapGL {...viewport} onViewportChange={setViewport} />
+      <button onClick={goToNYC}>New York City</button>
     </div>
   );
 }
-
-export default App;
